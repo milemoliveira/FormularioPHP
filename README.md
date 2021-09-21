50cf4636f1ad92900ccfde58891d67d1f95bfef0
Milena <milemoliveira@gmail.com>
2021-09-21 20:17:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Formulário com PHP
+
+**Exercício:** formulário simples que coleta os dados pessoais de uma pessoa e, em seguida, apresenta os dados em uma nova página pelo método **POST**.
+
+#### **Página do formulário**
+![Tela Formulário](form.png)
---
+++ form/formdata.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Nome:" . $_POST['nome'], "<br>";
+echo "Endereço: " . $_POST['endereco'] . $_POST['bairro'] . $_POST['cidade'] . $_POST['estado'], "<br>";
+echo "CEP: " . $_POST['cep'], "<br>";
+echo "Telefone: " . $_POST['telefone'], "<br>";
+echo "Email: " . $_POST['email'];
+
+?>
+
